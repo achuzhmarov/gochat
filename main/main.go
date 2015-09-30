8ba23f6f3f65c4348861686091d4e3504865dd88
artem.chuzhmarov <artem.chuzhmarov@gmail.com>
2015-09-30 19:14:39 +0300

    test

--- main/main.go
+++ main/main.go
@@ -8,31 +8,9 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/weekface/mgorus"
-
-	/*"path/filepath"
-	"os"
-	"fmt"
-	"io/ioutil"*/
 )
 
 func main() {
-	/*dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dir)
-	fmt.Println(os.Args[0])
-
-	files, _ := ioutil.ReadDir("./")
-	for _, f := range files {
-		fmt.Println(f.Name())
-	}
-
-	files, _ = ioutil.ReadDir("/")
-	for _, f := range files {
-		fmt.Println(f.Name())
-	}*/
-
 	hooker, err := mgorus.NewHooker(dal.DB_URL, dal.DB_NAME, "logs")
 	if err == nil {
 		log.AddHook(hooker)
